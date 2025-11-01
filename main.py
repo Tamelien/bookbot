ff12ce19fdca0cedea885612ce9e0b1a8783e163
Tamelien <Tamelien@proton.com>
2025-11-01 22:35:46 +0100

    update stats.py with function count_char

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_char
 
 def get_book_text(file_path):
     with open(file_path) as book:
@@ -9,6 +10,7 @@ def main():
     path_to_book = "books/frankenstein.txt"
     text = get_book_text(path_to_book)
     print(f"Found {count_words(text)} total words")
+    print(count_char(text))
 
 if __name__ == "__main__":  
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def count_words(text):
     return len(text.split())
 
+def count_char(text):
+    char_frequency = {}
+    for char in text:
+        char = char.lower()
+
+        if char in char_frequency:
+            char_frequency[char] += 1
+        else:
+            char_frequency[char] = 1 
+    
+    return char_frequency
