4e6d5f9ab33618d548d39122ef058abea0dbb702
Tamelien <Tamelien@proton.com>
2025-11-01 22:33:30 +0100

    add stats.py with function count_words

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_words
+
 def get_book_text(file_path):
     with open(file_path) as book:
         book_content = book.read()
@@ -6,7 +8,7 @@ def get_book_text(file_path):
 def main():
     path_to_book = "books/frankenstein.txt"
     text = get_book_text(path_to_book)
-   
+    print(f"Found {count_words(text)} total words")
 
 if __name__ == "__main__":  
     main()
