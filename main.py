f305c3b2d71065eb9e460b6c4ea431b0ada0c534
Tamelien <Tamelien@proton.com>
2025-11-01 23:20:38 +0100

    update main.py with path to book over command line argument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import count_words
 from stats import count_char
 from stats import sorted_char_frequency
+from sys import argv
+from sys import exit
 
 def get_book_text(file_path):
     with open(file_path) as book:
@@ -8,10 +10,20 @@ def get_book_text(file_path):
     return book_content
 
 def main():
-    path_to_book = "books/frankenstein.txt"
+
+    if  len(argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        exit(1)
+
+    path_to_book = argv[1]
+
     text = get_book_text(path_to_book)
     num_words = count_words(text)    
     frequency = sorted_char_frequency(count_char(text))
+    print_report(path_to_book, num_words, frequency)
+    
+
+def print_report(path_to_book, num_words, frequency):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path_to_book}...")
     print("----------- Word Count ----------")
