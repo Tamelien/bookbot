7447e9b99323ad0a763ecb7ff924d282afcf7552
Tamelien <Tamelien@proton.com>
2025-11-01 22:51:03 +0100

    update stats with sorted_char_frequency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_char
+from stats import sorted_char_frequency
 
 def get_book_text(file_path):
     with open(file_path) as book:
@@ -9,8 +10,11 @@ def get_book_text(file_path):
 def main():
     path_to_book = "books/frankenstein.txt"
     text = get_book_text(path_to_book)
-    print(f"Found {count_words(text)} total words")
-    print(count_char(text))
+    num_words = count_words(text)    
+    frequency = sorted_char_frequency(count_char(text))
+
+    print(f"Found {num_words} total words")
+    print(f"{frequency}")
 
 if __name__ == "__main__":  
     main()
--- stats.py
+++ stats.py
@@ -12,3 +12,16 @@ def count_char(text):
             char_frequency[char] = 1 
     
     return char_frequency
+
+def sort_on(items):
+    return items["num"]
+
+def sorted_char_frequency(char_frequency):
+    sorted_list = []
+
+    for key in char_frequency:
+        sorted_list.append({"char": key, "num": char_frequency[key]})
+
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
