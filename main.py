b5de097a0a608c6fd63fc1e95d0812525920190e
Tamelien <Tamelien@proton.com>
2025-11-01 22:23:50 +0100

    update .gitignore, main.py with function get_book_text

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+def get_book_text(file_path):
+    with open(file_path) as book:
+        book_content = book.read()
+    return book_content
+
 def main():
-    pass
+    path_to_book = "books/frankenstein.txt"
+    text = get_book_text(path_to_book)
+   
 
 if __name__ == "__main__":  
     main()
